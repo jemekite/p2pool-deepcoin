a4391a000631b3cbfd24ad2d6fbc26e39c453414
Abid Kecil <hamdi@hamdi.web.id>
2014-07-08 22:48:12 +0700

    Update run.sh
    simply run this script (./run.sh) to start p2pool

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if ps ax | grep -v grep | grep "$SERVICE" > /dev/null
 then
 echo "$SERVICE is already running!"
 else
-screen -d -m -S P2P_DEEP python ./run_p2pool.py --net deepcoin --give-author 0 --disable-upnp -f 2
+screen -d -m -S P2P_DEEP python ./run_p2pool.py --net deepcoin --give-author 0 --disable-upnp -f 1
 
 wait
 fi
